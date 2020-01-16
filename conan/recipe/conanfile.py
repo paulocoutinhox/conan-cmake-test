d859842e302a2faa2e41026afda91a530f8378a3
Paulo Coutinho <paulo@prsolucoes.com>
2020-01-16 13:49:22 -0300

    initial commit

--- conan/recipe/conanfile.py
+++ conan/recipe/conanfile.py
@@ -16,7 +16,7 @@ class TargetConan(ConanFile):
     generators = "cmake"
 
     def build(self):
-        cmake = CMake(self, generator="Xcode")
+        cmake = CMake(self)
         cmake.definitions["CMAKE_BUILD_TYPE"] = self.settings.build_type
         cmake.definitions["PROJECT_CONFIG_ARCH"] = self.settings.arch
         cmake.definitions["PROJECT_CONFIG_GROUP"] = self.options.group
